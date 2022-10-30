2b3117786ddeab01b8dfb7f50af9b363b2d4d237
Piotr Kamecki <piotrkamecki8@gmail.com>
2022-10-30 13:38:16 +0100

    add html data and styles, styles to update

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,6 +1,10 @@
 export const settings = {
-    db: {
-      url: '//localhost:3131',
-      products: 'products',
-    },
-  };
+  db: {
+    url: '//localhost:3131',
+    products: 'products',
+  },
+};
+
+export const templates = {
+  articleLink: Handlebars.compile(document.querySelector('#template-article-link').innerHTML)
+};
